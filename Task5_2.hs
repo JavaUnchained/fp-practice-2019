922b2dee9d2a8da9165bb5286c609c0b92bd3699
PC <AndreyVladimirov1ch@yandex.ru>
2020-01-19 05:43:11 +0400

    Complited task

--- Task5_2.hs
+++ Task5_2.hs
@@ -1,12 +1,14 @@
 module Task5_2 where
 
 import Todo(todo)
-
+import Prelude hiding (concat)
 -- Зиппер из лекции 
 
 data Zipper a = Zipper [a] [a]
 
 -- Реализуйте экземпляры классов Show и Eq для этого типа
+instance (Show a) => Show (Zipper a) where
+    show (Zipper l r) = (show $ reverse l) ++ (show r)
 
 fromList :: [a] -> Zipper a
 fromList lst = Zipper [] lst
@@ -35,10 +37,23 @@ removeLeft (Zipper (_:lt) r) = Zipper lt r
 -- вставки подсписка в середину и выделения подсписка
 
 concat :: Zipper a -> Zipper a -> Zipper a
-concat left right = todo
+concat (Zipper l r) (Zipper l' r') = Zipper (l ++ r)  (l' ++ r')
+
+-- просто конкатенация зиппера в список
+innerCon :: Zipper a -> [a]
+innerCon (Zipper l r) = l ++ r
 
 insertManyAt :: Int -> Zipper a -> Zipper a -> Zipper a
-insertManyAt index what into = todo
+insertManyAt _ (Zipper [] []) into = into
+insertManyAt _ what (Zipper [] []) = what
+insertManyAt index w@(Zipper l r) i@(Zipper l' r') | index > (length $ innerCon i) || index < 0 = error"Incorrect index"
+                                                   | otherwise  = helper index w $ fromList $ innerCon i
+                                                    where
+                                                        helper ind w' in' | ind == 0  = Zipper (l' ++ l) (r ++ r')
+                                                                          | otherwise = helper (ind-1) w' (goRight in')
 
 subZipper :: Int -> Int -> Zipper a -> Zipper a
-subZipper from to input = todo
+subZipper _    _   (Zipper [] []) = error"Zipper is empty"
+subZipper from to i@(Zipper l r) | from < 0 || to < 0 || from > to || from > (ln i) || to > (ln i) = error"Incorrect from/to define"
+                                 | otherwise = Zipper [] (take (to - from + 1) $ drop from (l ++ r ))
+                                    where ln x = length $ innerCon x
