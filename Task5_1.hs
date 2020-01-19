3ff8963ec3277a32e7bd8caab873d00b2373ae75
PC <AndreyVladimirov1ch@yandex.ru>
2020-01-19 08:34:51 +0400

    Complited task

--- Task5_1.hs
+++ Task5_1.hs
@@ -33,11 +33,28 @@ list2dlist' left (h: t) =
 
 
 -- Реализуйте функции индексирования, вставки и удаления элементов
+
 index :: DList a -> Int -> a
-index = todo
+index DNil _ = error"Can't indexing. List is empty."
+index (DCons l c r) i | i <  0    = error"Index less then zero"
+                      | i == 0    = c  
+                      | otherwise = index r (i -1)
+
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt DNil index value | index == 0  = DCons DNil value DNil
+                          | otherwise   = error"Incorrect index for empty list"
+insertAt (DCons l c r) index value | index < 0  = error"Index less then zero"
+                                   | index > 0  = DCons l c (insertAt r (index - 1) value)
+                                   | index == 0 = m 
+                                        where
+                                            m = DCons l value n
+                                            n = DCons m c r
+
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt DNil _ = error"Can't remove. List is empty"
+removeAt (DCons _ _ DNil) 0 = DNil
+removeAt (DCons _ _ DNil) _ = error"Can't remove. This index out of bounds "
+removeAt (DCons l c r@(DCons _ c' r')) index | index == 0 = DCons l c' r'
+                                             | otherwise  = DCons l c $ removeAt r $ index -1
